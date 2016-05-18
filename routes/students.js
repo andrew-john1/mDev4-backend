e8dfbbd83e13494258a5227145e1db6e282d16ea
Nick <N.Shayan@me.com>
2016-05-18 16:54:25 +0200

    Added column sex.

--- routes/students.js
+++ routes/students.js
@@ -5,163 +5,171 @@ var Connection = require('tedious').Connection;
 var config = require('../config');
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-	// connect database
-	var connection = new Connection(config);
-	connection.on('connect', function(err) {
-		// If no error, then good to proceed.
-	    console.log("Connected");
-	    executeStatement();
-	});
+router.get('/', function (req, res) {
+    // connect database
+    var connection = new Connection(config);
+    connection.on('connect', function (err) {
+        // If no error, then good to proceed.
+        console.log("Connected");
+        executeStatement();
+    });
 
-	// execute a query
-	var Request = require('tedious').Request;
-	var TYPES = require('tedious').TYPES;
+    // execute a query
+    var Request = require('tedious').Request;
+    var TYPES = require('tedious').TYPES;
 
-	function executeStatement() {
-	    request = new Request("SELECT * FROM [LVS].[Student]", function(err) {
-	        if (err) {
-	            console.log("err: " + err);}
-	    });
+    function executeStatement() {
+        request = new Request("SELECT * FROM [LVS].[Student]", function (err) {
+            if (err) {
+                console.log("err: " + err);
+            }
+        });
 
-	    var resultTotal = [];
+        var resultTotal = [];
 
-	    var promise = new Promise(function(resolve, reject) {
-			
-	    	request.on('row', function(columns) {
+        var promise = new Promise(function (resolve, reject) {
 
-	    		var group = {};
+            request.on('row', function (columns) {
 
-		        columns
-		        	.map(function(row) {
+                var group = {};
 
-		        		switch(row.metadata.colName) {
-						    case "id":
-						        group.id = row.value;
-						        break;
-						    case "student_code":
-						        group.student_code = row.value;
-						        break;
-					        case "particulars":
-						        group.particulars = row.value;
-						        break;
-					        case "birth_date":
-						        group.birth_date = row.value;
-						        break;
-					        case "first_name":
-						        group.first_name = row.value;
-						        break;
-					        case "middle_name":
-						        group.middle_name = row.value;
-						        break;
-					        case "last_name":
-						        group.last_name = row.value;
-						        break;
-					        case "start_year":
-						        group.start_year = row.value;
-						        resultTotal.push(group);
-						        break;
-						    default:
-						        group.error = row.value;
-						}
-		        		
-			    	});
+                columns
+                    .map(function (row) {
 
-		    	resolve(resultTotal);	
-		    });
+                        switch (row.metadata.colName) {
+                            case "id":
+                                group.id = row.value;
+                                break;
+                            case "student_code":
+                                group.student_code = row.value;
+                                break;
+                            case "particulars":
+                                group.particulars = row.value;
+                                break;
+                            case "birth_date":
+                                group.birth_date = row.value;
+                                break;
+                            case "first_name":
+                                group.first_name = row.value;
+                                break;
+                            case "middle_name":
+                                group.middle_name = row.value;
+                                break;
+                            case "last_name":
+                                group.last_name = row.value;
+                                break;
+                            case "start_year":
+                                group.start_year = row.value;
+                                break;
+                            case "sex":
+                                group.sex = row.value;
+                                resultTotal.push(group);
+                                break;
+                            default:
+                                group.error = row.value;
+                        }
 
-	    });
+                    });
 
-	    promise.then(function(result) {
-	    	res.send(result);
-	    })
-	   
-	    request.on('done', function(rowCount, more) {
-	        console.log(rowCount + ' rows returned');
-	    });
-	    connection.execSql(request);
-	}
+                resolve(resultTotal);
+            });
+
+        });
+
+        promise.then(function (result) {
+            res.send(result);
+        })
+
+        request.on('done', function (rowCount, more) {
+            console.log(rowCount + ' rows returned');
+        });
+        connection.execSql(request);
+    }
 });
 
-router.get('/:id', function(req, res) {
-	var id = req.params.id;
+router.get('/:id', function (req, res) {
+    var id = req.params.id;
 
-	// connect database
-	var connection = new Connection(config);
-	connection.on('connect', function(err) {
-		// If no error, then good to proceed.
-	    console.log("Connected");
-	    executeStatement();
-	});
+    // connect database
+    var connection = new Connection(config);
+    connection.on('connect', function (err) {
+        // If no error, then good to proceed.
+        console.log("Connected");
+        executeStatement();
+    });
 
-	// execute a query
-	var Request = require('tedious').Request;
-	var TYPES = require('tedious').TYPES;
+    // execute a query
+    var Request = require('tedious').Request;
+    var TYPES = require('tedious').TYPES;
 
-	function executeStatement() {
-	    request = new Request(`SELECT * FROM [LVS].[Student] AS studentInfo WHERE studentInfo.id = ${id}`, function(err) {
-	        if (err) {
-	            console.log("err: " + err);}
-	    });
+    function executeStatement() {
+        request = new Request(`SELECT * FROM [LVS].[Student] AS studentInfo WHERE studentInfo.id = ${id}`, function (err) {
+            if (err) {
+                console.log("err: " + err);
+            }
+        });
 
-	    var resultTotal = [];
+        var resultTotal = [];
 
-	    var promise = new Promise(function(resolve, reject) {
-			
-	    	request.on('row', function(columns) {
+        var promise = new Promise(function (resolve, reject) {
 
-	    		var group = {};
+            request.on('row', function (columns) {
 
-		        columns
-		        	.map(function(row) {
+                var group = {};
 
-		        		switch(row.metadata.colName) {
-						    case "id":
-						        group.id = row.value;
-						        break;
-						    case "student_code":
-						        group.student_code = row.value;
-						        break;
-					        case "particulars":
-						        group.particulars = row.value;
-						        break;
-					        case "birth_date":
-						        group.birth_date = row.value;
-						        break;
-					        case "first_name":
-						        group.first_name = row.value;
-						        break;
-					        case "middle_name":
-						        group.middle_name = row.value;
-						        break;
-					        case "last_name":
-						        group.last_name = row.value;
-						        break;
-					        case "start_year":
-						        group.start_year = row.value;
-						        resultTotal.push(group);
-						        break;
-						    default:
-						        group.error = row.value;
-						}
-		        		
-			    	});
+                columns
+                    .map(function (row) {
 
-		    	resolve(resultTotal);	
-		    });
+                        switch (row.metadata.colName) {
+                            case "id":
+                                group.id = row.value;
+                                break;
+                            case "student_code":
+                                group.student_code = row.value;
+                                break;
+                            case "particulars":
+                                group.particulars = row.value;
+                                break;
+                            case "birth_date":
+                                group.birth_date = row.value;
+                                break;
+                            case "first_name":
+                                group.first_name = row.value;
+                                break;
+                            case "middle_name":
+                                group.middle_name = row.value;
+                                break;
+                            case "last_name":
+                                group.last_name = row.value;
+                                break;
+                            case "start_year":
+                                group.start_year = row.value;
+                                break;
+                            case "sex":
+                                group.sex = row.valueOf();
+                                resultTotal.push(group);
+                                break;
+                            default:
+                                group.error = row.value;
+                        }
 
-	    });
+                    });
 
-	    promise.then(function(result) {
-	    	res.send(result);
-	    })
-	   
-	    request.on('done', function(rowCount, more) {
-	        console.log(rowCount + ' rows returned');
-	    });
-	    connection.execSql(request);
-	}
-	
+                resolve(resultTotal);
+            });
+
+        });
+
+        promise.then(function (result) {
+            res.send(result);
+        })
+
+        request.on('done', function (rowCount, more) {
+            console.log(rowCount + ' rows returned');
+        });
+        connection.execSql(request);
+    }
+
 });
 
 module.exports = router;
